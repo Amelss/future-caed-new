97ecb532fcb7d0b32097874e1674c38c62614b31
Amelss <ameleyka@gmail.com>
2022-05-16 13:40:35 +0100

    adding of js to make images flip

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ const tooltips = document.querySelectorAll(".tt");
 tooltips.forEach((t) => {
   new bootstrap.Tooltip(t);
 });
+
+
+const selfSuff = document.querySelector(".self-suff");
+const selfSuffPic = document.querySelector(".self-pic")
+const flipPicBack = document.querySelector(".close-self-suff");
+
+
+
+
+
+const flipCircle = function () {
+  selfSuff.classList.remove('d-none');
+}
+
+const flipBack = function () {
+  selfSuff.classList.add("d-none")
+}
+
+selfSuffPic.addEventListener('click', flipCircle);
+flipPicBack.addEventListener('click', flipBack);
