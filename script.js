a7e27f1e06a5061f4e9b9bd4306f58dfd8b045a2
Amelss <ameleyka@gmail.com>
2022-05-18 22:47:09 +0100

    finished js for circle images and added modal

--- script.js
+++ script.js
@@ -7,18 +7,47 @@ tooltips.forEach((t) => {
 const selfSuff = document.querySelector(".self-suff");
 const selfSuffPic = document.querySelector(".self-pic")
 const flipPicBack = document.querySelector(".close-self-suff");
-
+const sustainability = document.querySelector(".sustainability");
+const susPic = document.querySelector(".sus-pic")
+const flipSusBack = document.querySelector(".close-sustainability")
+const generational = document.querySelector(".generation")
+const genPic = document.querySelector(".gen-pic")
+const flipPic = document.querySelector(".close-generation ");
 
 
 
 
 const flipCircle = function () {
   selfSuff.classList.remove('d-none');
+  
+  
 }
 
 const flipBack = function () {
   selfSuff.classList.add("d-none")
 }
 
+const flipSusCircle = function () {
+  sustainability.classList.remove("d-none")
+}
+
+const flipSusPicBack = function () {
+  sustainability.classList.add("d-none")
+}
+
+
+
+const flipGenPic = function () {
+  generational.classList.remove("d-none")
+}
+
+const flipGenPicBack = function () {
+  generational.classList.add("d-none")
+}
+
 selfSuffPic.addEventListener('click', flipCircle);
 flipPicBack.addEventListener('click', flipBack);
+susPic.addEventListener('click',flipSusCircle)
+flipSusBack.addEventListener('click', flipSusPicBack);
+genPic.addEventListener('click', flipGenPic)
+flipPic.addEventListener('click',flipGenPicBack )
